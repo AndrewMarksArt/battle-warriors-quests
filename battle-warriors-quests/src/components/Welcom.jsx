67649c33cad69ff02bc69f6cd6d43a23efba62f8
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-03-30 09:51:35 -0500

    removed template code from creating vite app, added main css, and components

---
+++ battle-warriors-quests/src/components/Welcom.jsx
@@ -0,0 +1,12 @@
+import "../css/main.css"
+
+export default function Welcome() {
+    return (
+        <>
+        <div className="wrapper">
+            <h1>Welcome to the new Battle Warriors Quest for Valhalla site!</h1>
+            <p>This will be were we post new game updates and you can get early access.</p>
+        </div>
+        </>
+    );
+}
