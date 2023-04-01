bc601926c67b6ef0e041f887b3d03ccfdccad2c0
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-04-01 17:14:39 -0500

    setting basic colors for weach element and fixing navbar and hero

--- battle-warriors-quests/src/components/Navbar.jsx
+++ battle-warriors-quests/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import "../css/main.css"
 export default function Navbar () {
     return (
         <>
-        <div className="site-title">
-            <h1>Battle Warriors</h1>
+        <div className="nav-bar">
+            <h1 className="nav-bar--title">Battle Warriors</h1>
         </div>
         </>
     );
