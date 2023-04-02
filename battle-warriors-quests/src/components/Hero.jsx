c4782e80bc681863e6f43f10c237ad0e688b6d02
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-04-02 03:21:53 -0500

    updated navbar, hero, and main css to fix alignment and colors to match basic layout structure of figma design.

--- battle-warriors-quests/src/components/Hero.jsx
+++ battle-warriors-quests/src/components/Hero.jsx
@@ -3,22 +3,20 @@ import "../css/main.css"
 export default function Hero() {
     return (
         <>
-        <div className="wrapper">
-            <div className="top-section-container">
-                <h1 className="headline">Quests for Valhalla</h1>
-                <p className="description">Embark on a legendary quest for glory and honor! Let the sagas sing of your triumph against the fearsome foes that threaten the realm!</p>
-            </div>
-            <div className="gateway-container">
+        <div className="main">
+            <h1 className="headline">Quests for Valhalla</h1>
+            <h2 className="subhead">
+                Embark on a legendary quest for glory and honor! 
+                Let the sagas sing of your triumph against the fearsome foes that threaten the realm!
+            </h2>
+            <div className="container">
                 <img src="./img/stone_gateway.png" className="main-img"></img>
-
-                <div className="cta-content">
-                    <input type="text" placeholder="enter your email" className="input-email"></input>
+                <div className="content">
+                    <input type="text" placeholder="enter your email" className="input-text"></input>
                     <button className="button-primary">Get Early Access!</button>
                     <button className="button-secondary">And Join Discord</button>
                 </div>
-
             </div>
-            
         </div>
         </>
     );
--- battle-warriors-quests/src/components/Navbar.jsx
+++ battle-warriors-quests/src/components/Navbar.jsx
@@ -4,16 +4,14 @@ import "../css/main.css"
 export default function Navbar () {
     return (
         <>
-        <header className="nav-header">
-            <div className="logo">
-                <h1 className="logo--text">Battle Warriors</h1>
+        <header className="navbar">
+            <div className="site-title">
+                Battle Warriors
             </div>
 
-            <nav className="nav">
-                <ul className="nav--list">
-                    <li className="nav--list--item"><a href="https://www.twitter.com/battlevikingnft">Twitter</a></li>
-                    <li className="nav--list--item"><a href="https://discord.gg/battle-warriors">Discord</a></li>
-                </ul>
+            <nav className="navbar-links">
+                <a href="https://www.twitter.com/battlevikingnft">Twitter</a>
+                <a href="https://discord.gg/battle-warriors">Discord</a>
             </nav>
         </header>
         </>
