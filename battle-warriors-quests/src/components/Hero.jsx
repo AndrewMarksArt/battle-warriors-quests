9d785fbfce07cbb1c05ac945ba2cc3a88a32348e
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-04-02 01:49:11 -0500

    updated nav bar and hero with all relavant content, started to adujst css but not finished.

--- battle-warriors-quests/src/components/Hero.jsx
+++ battle-warriors-quests/src/components/Hero.jsx
@@ -4,8 +4,21 @@ export default function Hero() {
     return (
         <>
         <div className="wrapper">
-            <h1>Welcome to the new Battle Warriors Quest for Valhalla site!</h1>
-            <p>This will be were we post new game updates and you can get early access.</p>
+            <div className="top-section-container">
+                <h1 className="headline">Quests for Valhalla</h1>
+                <p className="description">Embark on a legendary quest for glory and honor! Let the sagas sing of your triumph against the fearsome foes that threaten the realm!</p>
+            </div>
+            <div className="gateway-container">
+                <img src="./img/stone_gateway.png" className="main-img"></img>
+
+                <div className="cta-content">
+                    <input type="text" placeholder="enter your email" className="input-email"></input>
+                    <button className="button-primary">Get Early Access!</button>
+                    <button className="button-secondary">And Join Discord</button>
+                </div>
+
+            </div>
+            
         </div>
         </>
     );
--- battle-warriors-quests/src/components/Navbar.jsx
+++ battle-warriors-quests/src/components/Navbar.jsx
@@ -4,9 +4,18 @@ import "../css/main.css"
 export default function Navbar () {
     return (
         <>
-        <div className="nav-bar">
-            <h1 className="nav-bar--title">Battle Warriors</h1>
-        </div>
+        <header className="nav-header">
+            <div className="logo">
+                <h1 className="logo--text">Battle Warriors</h1>
+            </div>
+
+            <nav className="nav">
+                <ul className="nav--list">
+                    <li className="nav--list--item"><a href="https://www.twitter.com/battlevikingnft">Twitter</a></li>
+                    <li className="nav--list--item"><a href="https://discord.gg/battle-warriors">Discord</a></li>
+                </ul>
+            </nav>
+        </header>
         </>
     );
 }
